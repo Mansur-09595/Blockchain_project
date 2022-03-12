430a7694a05c4a2d5ff586a5aa0ecfb6bc665d59
Mansur-09595 <45181096+Mansur-09595@users.noreply.github.com>
2022-03-12 03:11:25 +0300

    add transaction and buy page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from tkinter.messagebox import NO
+import time
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
@@ -21,7 +21,6 @@ mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    #send_money("Mansur", "John", 20)
     return render_template('index.html')
 
 def is_logged_in(f):
@@ -91,6 +90,40 @@ def login():
 
     return render_template('login.html')
 
+@app.route("/transaction", methods = ['GET', 'POST'])
+@is_logged_in
+def transaction():
+    form = SendMoneyForm(request.form)
+    balance = get_balance(session.get('username'))
+
+    if request.method == 'POST':
+        try:
+            send_money(session.get('username'), form.username.data, form.amount.data)
+            flash("Money Sent!", "success")
+        except Exception as e:
+            flash(str(e), 'danger')
+
+        return redirect(url_for('transaction'))
+
+    return render_template('transaction.html', balance=balance, form=form, page='transaction')
+
+@app.route("/buy", methods = ['GET', 'POST'])
+@is_logged_in
+def buy():
+    form = BuyForm(request.form)
+    balance = get_balance(session.get('username'))
+
+    if request.method == 'POST':
+        try:
+            send_money("BANK", session.get('username'), form.amount.data)
+            flash("Purchase Successful!", "success")
+        except Exception as e:
+            flash(str(e), 'danger')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('buy.html', balance=balance, form=form, page='buy')
+
 @app.route("/logout")
 @is_logged_in
 def logout():
@@ -101,7 +134,10 @@ def logout():
 @app.route("/dashboard")
 @is_logged_in
 def dashboard():
-    return render_template('dashboard.html', session=session)
+    balance = get_balance(session.get('username'))
+    blockchain = get_blockchain().chain
+    ct = time.strftime("%I:%M %p")
+    return render_template('dashboard.html', balance=balance, session=session, ct=ct, blockchain=blockchain, page='dashboard')
 
 if __name__ =='__main__':
     app.secret_key = secret_key
--- forms.py
+++ forms.py
@@ -1,4 +1,5 @@
-from wtforms import Form, StringField, DecimalField, IntegerField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, PasswordField, validators
+
 
 class RegisterForm(Form):
     name = StringField('Full Name', [validators.Length(min=1,max=50)])
@@ -6,3 +7,12 @@ class RegisterForm(Form):
     email = StringField('Email', [validators.Length(min=6,max=50)])
     password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords do not match')])
     confirm = PasswordField('Confirm Password')
+
+
+class SendMoneyForm(Form):
+    username = StringField('Username', [validators.Length(min=4,max=25)])
+    amount = StringField('Amount', [validators.Length(min=1,max=50)])
+
+
+class BuyForm(Form):
+    amount = StringField('Amount', [validators.Length(min=1,max=50)])
